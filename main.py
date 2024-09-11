1bf08e0cf63911d7c280fac00973fbad351af41b
manunasci <nascimentomanuella73@gmail.com>
2024-09-11 11:18:20 -0300

    adicionando melhorias no codigo

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 from quiz import*
 
 if __name__ == '__main__':
-    q1 = Quiz()
-    q2 = Quiz()
+    q1 = Quiz(5,3)
+    q2 = Quiz(10,4)
+    q3 = Quiz(12,1)
 
     print(q1)
     print(q2)
+    print(q3)
+
     print('Testando metodos separados')
     print(q1.get_acertos())
     print(q1.get_erros())
-    print(q1.)
+    print(q1.calcular_pontos())
+
+    lista_qs = [q1, q2, q3]
+    a1 = Aluno(1,"Maria" ,lista_qs)
+    a2 = Aluno(2,"Renata", lista_qs)
+    print(a1)
+
+    d = Disciplina("POO", "Manuella", 2024, 4)
+    d = add_aluno(a1)
+    d = add_aluno(a2)
+    print(d)
--- quiz.py
+++ quiz.py
@@ -1,12 +1,9 @@
+from typing import List
 class Quiz:
-    disciplina: str
-    aluno: str
     __acertos: int
     __erros: int
 
     def __int__(self, d : str, a : str, acertos : int, erros : int):
-        self.disciplina = d
-        self.aluno = a
         self.__acertos = acertos
         self.__erros = erros
 
@@ -34,3 +31,49 @@ class Quiz2A(Quiz):
 class Quiz3A(Quiz):
     def calcular_pontos(self):
         return self.get_acertos() - 2 *(self.get_erros())
+class Aluno:
+    __matricula : int
+    __nome: str
+    __quizes : List[Quiz]
+
+    def __int__(self, mat : int, nom : str, kahoots : List[Quiz]):
+        self.__matricula = mat
+        self.__nome = nom
+        self.__quizes = kahoots
+
+    def __str__(self):
+        info = f'Matricula: {self.__matricula}\n'
+        info += f'Nome: {self.__nome}\n'
+        soma = 0
+        for q in self.__quizes:
+            soma += q.calcular_pontos()
+        info += f'Total de pontos {soma}\n'
+        return info
+
+    def get_matricula(self):
+        return self.__matricula
+
+class Diciplina:
+    __nome : str
+    __professor : str
+    __ano : int
+    __semestre : int
+    __alunos : list[Aluno] = []
+
+    def __init__(self, n : str, p : str, ano : int, semest : int):
+        self.__nome = n
+        self.__professor = p
+        self.__ano = ano
+        self.__semestre = semest
+
+    def add_aluno(self, a : Aluno):
+        for aluno in self.__alunos:
+            if aluno.get_matricula() == a.get_matricula():
+                raise Exception('Aluno já existe!')
+        self.__alunos.append(a)
+    def __str__(self):
+        info = f'Disciplina: {self.__nome} Professor: {self.__professor}\n'
+        info += f'Ano: {self.__ano} Semestre: {self.__semestre}\n'
+        for aluno in self.__alunos:
+            info += f'{aluno}'
+            return info
